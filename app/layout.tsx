57ad1def2d662c9291cf1a7cb8b1e04c432d78b7
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-01-08 06:17:20 +0900

    🎉 layoutを試験的に作ってみた

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,34 @@
-import './globals.css'
+import Image from "next/image";
+import "styles/globals.scss";
+import styles from "./layout.module.scss";
+
+import backgroundTV from "assets/tv.svg";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className={styles.container}>
+          <Image
+            src={backgroundTV}
+            alt=""
+            width={1920}
+            height={1080}
+            className={styles.bgImage}
+          />
+          <div className={styles.header}>
+            <p>toshi00.dev</p>
+          </div>
+          <div className={styles.main}>
+            <main>{children}</main>
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
