1c8fb41f3494d072c3b19f15b42de949c65bcac1
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-01-09 06:27:55 +0900

    🎉 aboutページを一旦実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,60 @@
-import { Inter } from '@next/font/google'
+import { Noto_Sans_JP } from '@next/font/google'
 import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+import Headline from 'components/Headline'
+
+import icon from 'assets/icon.svg'
+
+import styles from './page.module.scss'
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['japanese'],
+  weight: ['400', '500'],
+})
 
 export default function Home() {
-  return <div>ああああ</div>
+  return (
+    <div className={`${styles.container} ${notoSansJP.className}`}>
+      <div className={styles.info}>
+        <Image src={icon} alt="" width={250} height={250} />
+        <h1 className={styles.name}>とし / toshi00</h1>
+        <p className={styles.description}>
+          メタモンと一緒に
+          <br />
+          雰囲気でものづくりしています
+        </p>
+      </div>
+      <div className={styles.article}>
+        <div>
+          <div>
+            <Headline title="プロフィール" />
+            <ul>
+              <li>東京工業大学 情報工学系 学部3年</li>
+              <li>デジタル創作同好会traP</li>
+            </ul>
+          </div>
+          <div>
+            <Headline title="趣味" />
+            <ul>
+              <li>電子工作</li>
+              <li>メタモン</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <Headline title="言語とか" />
+            <ul>
+              <li>React / Vue</li>
+              <li>Go</li>
+              <li>Arduino / C</li>
+            </ul>
+          </div>
+          <div>
+            <Headline title="リンク" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
