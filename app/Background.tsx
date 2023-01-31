a805a4b8d1c1510f3191c3cc3ef04a254ed3743f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-02-01 01:10:57 +0900

    🎨 画面が大きすぎるときの背景色に対応

--- app/Background.tsx
+++ app/Background.tsx
@@ -21,36 +21,38 @@ const Background = ({ page, header, footer }: Props) => {
     setIsZooming((prev) => !prev)
   }, [])
   return (
-    <div className={isZooming ? styles.zoomContainer : styles.container}>
-      <Image
-        src={backgroundTV}
-        alt=""
-        width={1920}
-        height={1080}
-        className={styles.bgImage}
-        draggable={false}
-        priority
-      />
-      <div className={styles.header}>{header}</div>
-      <div className={styles.mainWrapper}>
-        <div className={styles.main}>
-          <main className={styles.article}>
-            <div className={styles.pageContainer}>{page}</div>
-          </main>
-          <Image
-            src={rukachi}
-            alt=""
-            width={581}
-            height={662}
-            className={styles.rukachi}
-            draggable={false}
-            priority
-          />
+    <div className={isZooming ? styles.zoomBgColor : styles.bgColor}>
+      <div className={isZooming ? styles.zoomContainer : styles.container}>
+        <Image
+          src={backgroundTV}
+          alt=""
+          width={1920}
+          height={1080}
+          className={styles.bgImage}
+          draggable={false}
+          priority
+        />
+        <div className={styles.header}>{header}</div>
+        <div className={styles.mainWrapper}>
+          <div className={styles.main}>
+            <main className={styles.article}>
+              <div className={styles.pageContainer}>{page}</div>
+            </main>
+            <Image
+              src={rukachi}
+              alt=""
+              width={581}
+              height={662}
+              className={styles.rukachi}
+              draggable={false}
+              priority
+            />
+          </div>
         </div>
-      </div>
-      <div className={styles.footer}>{footer}</div>
-      <div className={styles.toggle}>
-        <FullScreenButton isFull={isZooming} onClick={onChange} />
+        <div className={styles.footer}>{footer}</div>
+        <div className={styles.toggle}>
+          <FullScreenButton isFull={isZooming} onClick={onChange} />
+        </div>
       </div>
     </div>
   )
