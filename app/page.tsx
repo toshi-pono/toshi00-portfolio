8b9b4a21a45e4cfcc8d9de65d0c958747955674f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-02-07 20:34:44 +0900

    ✨ aboutの中身をスマホ対応した

--- app/page.tsx
+++ app/page.tsx
@@ -30,53 +30,52 @@ export default function Home() {
       </div>
       <div className={styles.article}>
         <div>
-          <div>
-            <Headline title="プロフィール" />
-            <ul>
-              <li>東京工業大学 情報工学系 学部3年</li>
-              <li>デジタル創作同好会traP</li>
-            </ul>
-          </div>
-          <div>
-            <Headline title="趣味" />
-            <ul>
-              <li>電子工作</li>
-              <li>メタモン</li>
-            </ul>
-          </div>
+          <Headline title="プロフィール" />
+          <ul>
+            <li>東京工業大学 情報工学系 B3</li>
+            <li>デジタル創作同好会traP</li>
+          </ul>
         </div>
         <div>
-          <div>
-            <Headline title="言語とか" />
-            <ul>
-              <li>React / Vue</li>
-              <li>Go</li>
-              <li>Arduino / C</li>
-            </ul>
+          <Headline title="言語とか" />
+          <p>React / Vue / Go / Arduino / C</p>
+        </div>
+        <div>
+          <Headline title="実績" />
+          <ul>
+            <li>電子工作</li>
+            <li>メタモン</li>
+          </ul>
+        </div>
+        <div>
+          <Headline title="リンク" />
+          <div className={styles.links}>
+            <a
+              href="https://github.com/toshi-pono"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.link}
+            >
+              <IoLogoGithub className={styles.icon} />
+              Github
+            </a>
+            <a
+              href="https://twitter.com/toshi00_p"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.link}
+            >
+              <IoLogoTwitter className={styles.icon} />
+              Twitter
+            </a>
           </div>
-          <div>
-            <Headline title="リンク" />
-            <div className={styles.links}>
-              <a
-                href="https://github.com/toshi-pono"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.link}
-              >
-                <IoLogoGithub className={styles.icon} />
-                Github
-              </a>
-              <a
-                href="https://twitter.com/toshi00_p"
-                target="_blank"
-                rel="noreferrer"
-                className={styles.link}
-              >
-                <IoLogoTwitter className={styles.icon} />
-                Twitter
-              </a>
-            </div>
-          </div>
+        </div>
+        <div className={styles.phoneOnly}>
+          <Headline title="制作物" />
+          <ul>
+            <li>電子工作</li>
+            <li>メタモン</li>
+          </ul>
         </div>
       </div>
     </div>
