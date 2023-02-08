4fd4624762a7fbb36bf0e3dc7a979ebdd65877d4
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-02-08 18:49:31 +0900

    ✨ ログの表示

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -1,7 +1,19 @@
+import { logs } from 'data/logs'
+
+import LogCard from './LogCard'
+import styles from './page.module.scss'
+
 const Logs = () => {
   return (
-    <div>
-      <h1>Logs</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Logs</h1>
+      <ul className={styles.logs}>
+        {logs.map((log) => (
+          <li key={log.id}>
+            <LogCard log={log} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
