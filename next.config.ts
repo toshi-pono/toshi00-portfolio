ee648d169af4d4b96b4f9a01217c27443d29cd3f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2025-08-03 12:32:12 +0900

    設定の修正

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
   sassOptions: {
     additionalData: `@use "${path.resolve(__dirname, 'src/styles/variables.scss')}" as *;`,
   },
